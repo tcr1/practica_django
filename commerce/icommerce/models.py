012c47b994af6c59992ecc72f257ad4d2ed0b6b6
tania <taniacr94@gmail.com>
2016-04-03 18:20:19 +0200

    models_1.1.2

--- commerce/icommerce/models.py
+++ commerce/icommerce/models.py
@@ -11,7 +11,7 @@ class Botiga (models.Model):
     adress=models.TextField(blank=True, null=True)
 
     def __unicode__(self):
-        return u"%s" % self.name
+        return u"%s" % self.id_botiga + " " + self.nom_botiga
 
     def get_absolute_url(self):
         return reverse('commerce:botiga_detail', kwargs={'pk': self.pk})
@@ -31,14 +31,15 @@ class Marca(models.Model):
 
 class Pesa_roba(models.Model):
     RATING_TIPUS = ((1,'pantalo'),(2,'jersei'),(3,'blusa'),(4,'camiseta'),(5,'jaqueta'),(6,'Roba_interior'),(7,'short'))
-    tipus = models.PositiveSmallIntegerField('Rating (stars)',blank=False, default=1, tipus=RATING_TIPUS)
-    RATING_COLORS = ((1,'WHITE'),(2,'PINK'),(3,'BLACK'),(4,'BLUE'),(5,'BROWN'))
-    colors = models.PositiveSmallIntegerField('Rating (stars)',blank=False, default=1,colors=RATING_COLORS)
+    tipus = models.PositiveSmallIntegerField('Rating (stars)',blank=False, default=1, choices=RATING_TIPUS)
+    #RATING_COLORS = ((1,'WHITE'),(2,'PINK'),(3,'BLACK'),(4,'BLUE'),(5,'BROWN'))
+    #colors = models.PositiveSmallIntegerField('Rating (stars)',blank=False, default=1,choices=RATING_COLORS)
     codi_pesa = models.IntegerField()
     nom = models.TextField()
     preu = models.DecimalField('Euro amount',max_digits=8,decimal_places=2,blank=True,null=True)
     descripcio = models.TextField(blank=True,null=True)
-    talla = models.IntegerField()
+    RATING_Talla = ((1,'xs'),(2,'S'),(3,'M'),(4,'L'),(5,'XL'),(6,'XXL')
+    talla = models.PositiveSmallIntegerField('Rating (stars)', blank=False, defult=1, choices=RATING_Talla)
     imatge = models.ImageField(upload_to="commerce",blank=True,null=True)
 
 class Ciutat(models.Model):
