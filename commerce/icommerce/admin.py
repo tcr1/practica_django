8813a67b7795444a8299ab66267aca8fc522ac0d
tania <taniacr94@gmail.com>
2016-04-03 12:41:05 +0200

    models_1.1

--- commerce/icommerce/admin.py
+++ commerce/icommerce/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+import models
+
+admin.site.register(models.Botiga)
+admin.site.register(models.Marca)
+admin.site.register(models.Pesa_roba)
+admin.site.register(models.Ciutat)
