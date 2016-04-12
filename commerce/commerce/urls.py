8b9755800b0b7738507c165f885422551fdc78d4
cclauwers <cclauwers@gmail.com>
2016-04-13 01:37:20 +0200

    commit_cindy#3_logout

--- commerce/commerce/urls.py
+++ commerce/commerce/urls.py
@@ -8,8 +8,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
+    url(r'^$', mainpage, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
     url(r'^icommerce/', include('icommerce.urls', namespace='icommerce')),
-    url(r'^$', mainpage ,name='home'),
+
 )
